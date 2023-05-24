daaebff25c93bfaed6be1faed0fc490d08a340f7
DimaTykva <dimatykvadanait@gmail.com>
2023-05-24 19:05:34 +0300

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,30 @@
 import { Outlet } from "react-router-dom";
+import {useState} from "react";
+import SignIn from "../../modals/SignIn/SignIn";
 
 const Header = () => {
+
+  const [modalState, setModalState] = useState(false);
+
+
+  const openRegModal = () => {
+    console.log('hello ')
+    setModalState(!modalState)
+  }
+
   return <>
-    <div>HEADER</div>
+    <div className='header'>
+      <h2>HEADER</h2>
+      <button className='sign_in_btn' onClick={openRegModal}>Sign in</button>
+    </div>
+
+
+    {
+        (modalState) ? <SignIn openRegModal={openRegModal}/> : null
+    }
+
+
+
     <Outlet />
   </>
 
